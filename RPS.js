6d0f1301b0160506a6bc1634155224e6d1b50977
Noble <nobleugwuja@gmail.com>
2022-04-13 18:52:50 +0100

    added rps ui

--- RPS.js
+++ RPS.js
@@ -1,5 +1,14 @@
 let playerScore = 0;
 let computerScore = 0;
+
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let playerScoreEl = document.getElementById("player-score");
+let computerScoreEl = document.getElementById("computer-score");
+let result = document.getElementById("result");
+let compSelect = document.getElementById("comSelect");
+
+
 function computerPlay(){
     let gameSetUp = ["Rock", "Paper", "Scissors"];
     let randomEl = Math.floor(Math.random() * gameSetUp.length);
@@ -15,11 +24,9 @@ function playRound(playerSelection, computerSelection){
         return "You Won"
    }else if(playerSelection === "Scissors" && computerSelection === "Paper"){
         playerScore += 1;
-    
     return "You Won"
    }else if(playerSelection == "Paper" && computerSelection == "Rock"){
         playerScore += 1;
-
        return "You won"
     }else if (playerSelection === computerSelection){
         return "TIE"
@@ -27,31 +34,43 @@ function playRound(playerSelection, computerSelection){
         computerScore += 1;
        return "Computer Won"
    };
-    
-
-   
+  
 }
 
 let playerSelection = "";
 let computerSelection = "";
 
-
-
-
 function game() {
-   for (let i = 0; i < 5; i++) {
-    playerSelection = prompt("Enter a weapon");
-    console.log(playerSelection)
-    computerSelection = computerPlay();
-    console.log(computerSelection)
-    console.log(playRound(playerSelection, computerSelection))
-    console.log(playerScore, computerScore)
-
-       
-   }
+   
+    
+    btn1.addEventListener("click", () => {
+        playerSelection = "Rock"
+        computerSelection = computerPlay()
+        compSelect.textContent = computerSelection
+        result.textContent = playRound(playerSelection, computerSelection)
+        playerScoreEl.textContent = playerScore
+        computerScoreEl.textContent = computerScore
+        
+    })
+    btn2.addEventListener("click", () => {
+        playerSelection = "Paper"
+        computerSelection = computerPlay()
+        compSelect.textContent = computerSelection
+        result.textContent = playRound(playerSelection, computerSelection)
+        playerScoreEl.textContent = playerScore
+        computerScoreEl.textContent = computerScore
+    })
+    btn3.addEventListener("click", () => {
+        playerSelection = "Scissors"
+        computerSelection = computerPlay()
+        compSelect.textContent = computerSelection
+        result.textContent = playRound(playerSelection, computerSelection)
+        playerScoreEl.textContent = playerScore
+        computerScoreEl.textContent = computerScore
+    })
+   
 
 }
-
 game()
 
 
